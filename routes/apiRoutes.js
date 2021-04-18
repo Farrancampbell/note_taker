ddaf82a239573b9c477ca283f0083e27b43420d2
Farrancampbell <farranerinandcrew@gmail.com>
2021-04-18 11:43:33 -0400

    Updated Code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,36 @@
+const fs = require("fs")
+const { request } = require("http")
 var path = require("path")
 
 var router = require("express").Router()
-router.get("/api/notes", (require, response)=>{
-    response.sendFile(path.join(__dirname,"./db/db.json"))
+router.get("/notes", (require, response)=>{
+    fs.readFile(path.join(__dirname,"../db/db.json"),"utf8",(error,data)=>{
+        response.json(JSON.parse(data))
+
+    })
 })
 
-router.get("*", (require, response)=>{
-    response.sendFile(path.join(__dirname,"../public/index.html"))
+router.post("/notes", (require, response)=>{
+    response.sendFile(path.join(__dirname,"../db/db.json"))
 })
+
+router.post("/notes"), (require, response) => {
+    fs.writeFile("db/db.json", JSON.stringify(request.body), (err) => {
+        if (err) throw err;
+        response.json("success");
+    });
+
+router.delete("/notes/:id", (require, response) => {
+    fs.readFile("../db/db.json", (err, data) => {
+       const notesArray = JSON.parse(data);
+       const idNumber = require.params.id;
+       const filterArray = notesArray.filter(note => note.id !== idNumber);
+
+       fs.writeFile("../db/db.json", JSON.stringify(filterArray), "utf8");
+       response.sendStatus(200);
+
+    }
+}
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const apiRoutes = require("./routes/apiRoutes.js")
 const htmlRoutes = require("./routes/htmlRoutes");
 const app = express()
 const PORT = 8080
@@ -7,6 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use(express.static("public"))
+app.use("/api", apiRoutes)
 app.use("/",htmlRoutes)
 
 app.listen(PORT,() => console.log(`listening on port ${PORT}`))
