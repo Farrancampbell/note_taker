c2b10f9219ab0755f140fec187403dd1a1e3df1b
Farrancampbell <farranerinandcrew@gmail.com>
2021-04-19 09:33:51 -0400

    Changed Port to push to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const apiRoutes = require("./routes/apiRoutes.js")
 const htmlRoutes = require("./routes/htmlRoutes");
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
