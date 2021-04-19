cfd387d5d280887f398c481f12e69bd60cbd4ad5
Farrancampbell <farranerinandcrew@gmail.com>
2021-04-18 22:16:20 -0400

    Finally got it to delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -30,12 +30,12 @@ router.post("/notes", (req, response) => {
     })
 })
 router.delete("/notes/:id", (require, response) => {
-    fs.readFile("../db/db.json", (data) => {
+    fs.readFile(path.join(__dirname,"../db/db.json"),(err,data)=> {
        const notesArray = JSON.parse(data);
        const idNumber = require.params.id;
        const filterArray = notesArray.filter(note => note.id !== idNumber);
 
-       fs.writeFile("../db/db.json", JSON.stringify(filterArray), "utf8");
+       fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(filterArray), "utf8", (err) => { console.log(err) });
        response.sendStatus(200);
     
     })
